ece9b2f4e1c994c7ce67ab2610714f86f28be5db
Morshedul Arefin <arefin@Morsheduls-MacBook-Air.local>
2023-05-03 17:33:30 +0600

    echo and print

---
+++ 6. echo and print/index.php
@@ -0,0 +1,10 @@
+<?php
+//echo "My country name is Bangladesh";
+//echo("My country name is Bangladesh");
+//echo ""<h1>Bangladesh</h1>;
+//echo "Bangladesh ","is ","my country";
+
+//print "My country name is Bangladesh";
+//print("My country name is Bangladesh");
+//print "<h1>Bangladesh</h1>";
+//print "Bangladesh ","is ","my country";
