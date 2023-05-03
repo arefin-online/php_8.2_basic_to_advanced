0487bdf997d6f105c307159576aeefcc8f3d31ac
Morshedul Arefin <arefin@Morsheduls-MacBook-Air.local>
2023-05-03 18:22:19 +0600

    Order By Clause

---
+++ 40. Order_By_Clause/index.php
@@ -0,0 +1,33 @@
+<?php
+$dbhost = 'localhost';
+$dbname = 'testdb001';
+$dbuser = 'root';
+$dbpass = '';
+try {
+    $pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch( PDOException $exception ) {
+    echo "Connection error :" . $exception->getMessage();
+}
+
+$q = $pdo->prepare("SELECT * FROM students WHERE id>=4 ORDER BY id ASC");
+$q->execute();
+$result = $q->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<table>';
+echo '<tr><th>Id</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Phone</th></tr>';
+foreach($result as $row) {
+    echo '<tr>';
+    echo '<td>'.$row['id'].'</td>';
+    echo '<td>'.$row['firstname'].'</td>';
+    echo '<td>'.$row['lastname'].'</td>';
+    echo '<td>'.$row['email'].'</td>';
+    echo '<td>'.$row['phone'].'</td>';
+    echo '</tr>';
+}
+echo '</table>';
+
+// echo "<pre>";
+// print_r($result);
+// echo "</pre>";
